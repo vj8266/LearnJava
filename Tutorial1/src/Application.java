45c73caad51750287622ceaa6155c09600d3fbcf
venkataramanj <65354632+vj8266@users.noreply.github.com>
2020-05-27 12:37:24 -0500

    Commit

---
+++ Tutorial1/src/Application.java
@@ -0,0 +1,31 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// System.out.println("Hello World!");
+		int myNumber = 88;
+		short myShort = 123;
+		long myLong = 9876;
+		double myDouble = 7.14321;
+		float myFloat = 345.3f;
+		char myChar = 'y';
+		boolean myBoolean = false;
+		byte myByte = 127;
+
+		String text = "Hello";
+		String blank = " ";
+		String name = "Venky";
+
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+		System.out.println(text + blank + name);
+		System.out.println("My integer is:" + myNumber);
+	}
+
+}
